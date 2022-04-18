bfdd0973405f93082831528c9f49aad4dd90add5
Davi Batista Tonon <tonon1996@gmail.com>
2022-04-17 21:18:11 -0300

    Leitura de arquivos CSV

--- controller/views.py
+++ controller/views.py
@@ -9,13 +9,20 @@ def index(request):
 
     if request.method == 'POST':
         form = FormFileUpload(request.POST)
-       
         file = request.FILES['arquivo']
-        size = file.size/1048576
-      
-        print('Nome:', file.name)
-        print('Tamanho (MB): {:.2f}'.format( size))
+        ler_arquivo(file)
     else:
         form = FormFileUpload()
 
     return render(request, 'index.html', {'form': form})
+
+
+def ler_arquivo(file):
+    """Ler um arquivo e exibir nome, tamanho e cada linha do arquivo"""
+
+    size = file.size/1048576
+    print('Nome:', file.name)
+    print('Tamanho (MB): {:.2f}'.format( size))
+    
+    for chunk in file.chunks():
+        print(chunk.decode('utf-8'))
