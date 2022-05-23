b3ff0186272d5d7d287bbf760336e84003d1c5f1
Davi Batista Tonon <tonon1996@gmail.com>
2022-05-23 20:36:59 -0300

    .

--- apps/usuarios/test/test_urls.py
+++ apps/usuarios/test/test_urls.py
@@ -11,17 +11,9 @@ from django.contrib.sessions.middleware import SessionMiddleware
 class UsuariosUrlsTestcase(TestCase):
     
     def setUp(self):
-        nome = 'teste'
-        email = 'teste@email.com'
-        senha = '203050'
-
         self.factory = RequestFactory()
         self.cliente = Client()
 
-        # self.user = User.objects.create_user(
-        #     username=nome, 
-        #     email=email, 
-        #     password=senha)
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create_user(
@@ -32,36 +24,43 @@ class UsuariosUrlsTestcase(TestCase):
 
     def test_login_usuarios_get(self):
         """ Teste get da pagina de login de usuarios"""
-        request = self.factory.get('login')
 
+        request = self.factory.get('login')
         with self.assertTemplateUsed('login.html'):
             response = login(request)
             self.assertEqual(response.status_code, 200)
 
     def test_login_usuarios_post(self):
+        """Testa quando um usuario faz login.
+        verificado se e redirecionado para pagina index"""
+
         post_request = self.cliente.post('/login',
         {  'password':203050, 'email':self.user.email}, follow=True)
         self.assertRedirects(post_request, '/')
 
     def test_cadastro_usuarios_get(self):
         """ Teste a requisicao GET da pagina de cadastro de usuarios"""
-        request = self.factory.get('cadastro')
 
+        request = self.factory.get('cadastro')
         with self.assertTemplateUsed('cadastros.html'):
             response = cadastro(request)
             self.assertEqual(response.status_code, 200)
     
-    def test_cadastro_usurios_post(self):
+    def test_cadastro_usuarios_post(self):
         """Testa a requisicao POST da pagina de cadastros"""
+
         post_request = self.client.post(
             '/cadastro',
             {'nome': 'davi', 'email': 'davi@gmail.com'})
         self.assertRedirects(post_request, '/', target_status_code=302)
 
     def test_lista_de_usuarios(self):
-        request = self.factory.get('lista_usuarios')
-        self.client.login(password=self.user.password, username=self.user.username)
+        """Testa as requisição do templates da lista de usuarios"""
+
+        self.cliente.login(username='teste',password='203050')
         request = self.cliente.get('/lista_usuarios')
-        with self.assertTemplateUsed('lista_usuarios.html'):
-            response = lista_usuarios(request)
-            self.assertEqual(response.status_code, 200)
+       
+        self.assertTemplateUsed(request, 'lista_usuarios.html')
+        self.assertEqual(request.status_code, 200)
+        self.assertEqual(request.status_code, 200)
+ 
